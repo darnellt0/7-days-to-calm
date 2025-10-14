1e2ad65dea1ed69bfe75355928bd536c988df28d
Darnell Tomlinson <darnellto@yahoo.com>
2025-10-14 13:18:45 -0700

    Allow mic via Permissions-Policy header

--- em-frontend/next.config.js
+++ em-frontend/next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            // let the page use the microphone (unblocks getUserMedia)
+            key: 'Permissions-Policy',
+            value: 'microphone=(self)',
+            // If you prefer to be explicit, you can use:
+            // value: 'microphone=(self "https://elevenlabs.io" "https://api.elevenlabs.io")'
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
